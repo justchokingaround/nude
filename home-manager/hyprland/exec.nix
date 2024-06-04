cd225d33d356999bc587d879475dc82bacfc1321
justchokingaround <ivanonarch@tutanota.com>
2024-06-04 03:31:56 +0200

    new: add wallpapers

--- home-manager/hyprland/exec.nix
+++ home-manager/hyprland/exec.nix
@@ -4,7 +4,7 @@ in {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "swww kill ; swww init"
-      "swww img ~/nude/modules/shared/wallpapers/oxocarbon.png --transition-type random --transition-step 5 --transition-fps 165"
+      "swww img ~/nude/modules/shared/wallpapers/nix.png --transition-type random --transition-step 5 --transition-fps 165"
       "vesktop"
       "[workspace 2] schizofox"
       "${scripts.socket}"
