00f19049b83b388f9f2f1af352c278cf1e1efec7
justchokingaround <ivanonarch@tutanota.com>
2024-06-26 04:59:55 +0200

    remove(xdg): kde portal

--- modules/workstation/options/default.nix
+++ modules/workstation/options/default.nix
@@ -6,7 +6,7 @@ in {
   options = {
     xdg.portal.name = mkOption {
       description = "The name of the xdg desktop portal, used both in the package name and to specify the default portal";
-      type = types.enum ["wlr" "gtk" "xapp" "gnome" "cosmic" "hyprland" "kde" "lxqt" "pantheon"];
+      type = types.enum ["wlr" "gtk" "xapp" "gnome" "cosmic" "hyprland" "lxqt" "pantheon"];
       default = "gtk";
     };
   };
