cc8ccb9d543da34c25c1f521d0a5c5c79c481a35
justchokingaround <ivanonarch@tutanota.com>
2024-08-05 23:44:29 +0200

    scripts: add clipboard history script

--- home-manager/hyprland/scripts.nix
+++ home-manager/hyprland/scripts.nix
@@ -18,13 +18,6 @@ in {
 
   config = {
     programs.hyprland.scripts = {
-      pin = writeShellScript "pin" ''
-        if ! hyprctl -j activewindow | ${jq} -e .floating; then
-          hyprctl dispatch togglefloating
-        fi
-        hyprctl dispatch pin
-      '';
-
       socket = writeShellScript "socket" ''
         ${getExe pkgs.socat} -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r event; do
           action="''${event%%>>*}"
@@ -62,6 +55,24 @@ in {
         		exit 0
         	fi
         done
+      '';
+
+      clipShow = writeShellScript "clipShow" ''
+        export CLIP=true
+        tmp_dir="/tmp/cliphist"
+        rm -rf "$tmp_dir"
+        mkdir -p "$tmp_dir"
+
+        read -r -d "" prog <<EOF
+        /^[0-9]+\s<meta http-equiv=/ { next }
+        match(\$0, /^([0-9]+)\s(\[\[\s)?binary.*(jpg|jpeg|png|bmp)/, grp) {
+            system("echo " grp[1] "\\\\\t | cliphist decode >$tmp_dir/"grp[1]"."grp[3])
+            print \$0"\0icon\x1f$tmp_dir/"grp[1]"."grp[3]
+            next
+        }
+        1
+        EOF
+        cliphist list | gawk "$prog" | rofi -dmenu -i -p '' -theme preview | cliphist decode | wl-copy
       '';
     };
   };
