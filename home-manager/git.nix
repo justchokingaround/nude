b2ccb570fdf5be09c12a617e50f78f97fd9edadb
justchokingaround <ivanonarch@tutanota.com>
2024-08-15 15:57:19 +0200

    git: git identity

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,4 +1,13 @@
-{pkgs, ...}: {
+{config, lib, pkgs, ...}:
+let
+    gitIdentity = pkgs.writeShellScriptBin "git-identity" (builtins.readFile ./git-identity);
+in
+{
+  home.packages = with pkgs; [
+    gitIdentity
+    fzf
+  ];
+
   programs.gh = {
     enable = true;
     extensions = [pkgs.gh-dash];
@@ -6,17 +15,33 @@
 
   programs.git = {
     enable = true;
-    userName = "justchokingaround";
-    userEmail = "ivanonarch@tutanota.com";
     delta.enable = true;
 
     extraConfig = {
+
+      # extremely important, otherwise git will attempt to guess a default user identity. see `man git-config` for more details
+      user.useConfigOnly = true;
+
+      user.github.name = "justchokingaround";
+      user.github.email = "ivanonarch@tutanota.com";
+
+      user.codeberg.name = "choker";
+      user.codeberg.email = "chomker@proton.me";
+
+      aliases = {
+        identity = "! git-identity";
+        id = "! git-identity";
+      };
+
       url = {
+        core = {
+          sshCommand = "ssh -i ~/.ssh/github_main.pub -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no";
+        };
         "git@github.com:" = {
           insteadOf = "https://github.com/";
         };
-        core = {
-          sshCommand = "ssh -i ~/.ssh/github_main.pub -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no";
+        "git@codeberg.org:" = {
+          insteadOf = "https://codeberg.org/";
         };
       };
     };
